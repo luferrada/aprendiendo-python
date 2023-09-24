fbbc7d4b4390579d077f819777db46adbd4d78c2
Lucas Ferrada <lucasferrada8@gmail.com>
2023-09-24 17:50:07 -0300

    Agrega un archivo con ejercicios de la primera semana

---
+++ ejercicios_semana_1.py
@@ -0,0 +1,31 @@
+# 1. Saludo personalizado:
+# Escribe tu nombre en una variable y luego imprime un saludo personalizado en
+# la consola usando esa variable.
+
+# 2. Edad futura:
+# Escribe tu edad en una variable y luego imprime tu edad en 5 años más.
+# Tambien en 8 y 13 años más.
+
+# 3. Suma de 2 números:
+# Ingresa 2 números en variables y luego imprime la suma de esos 2 números en
+# la consola.
+
+# 4. Cálculo de area de un rectángulo:
+# Ingresa la longitud y el ancho de un rectángulo en variables y luego imprime
+# el área del rectángulo en la consola.
+
+# 5. Conversión de monedas:
+# Ingresa una cantidad de dinero en pesos chilenos en una variable y luego
+# imprime la conversión a dólares en la consola.
+
+# 6. Números pares e impares:
+# Ingresa un número en una variable y luego imprime si es par o impar en la
+# consola.
+
+# 7. Índice de masa corporal:
+# Ingresa tu peso y tu altura en variables y luego imprime tu índice de masa
+# corporal en la consola.
+
+# 8. Propina de un restaurante:
+# Ingresa el total de la cuenta de un restaurante en una variable y luego
+# imprime el 10% de propina y el total de la cuenta con propina en la consola.
